3b5c7bb238bab2fdea5f69e84fdf0078deb71b66
unknown <mdsakiluzzaman@gmail.com>
2022-03-19 11:49:34 +0600

    react redux example fininshed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 
 import './App.css';
-
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from 'react';
+import {useSelector , useDispatch} from 'react-redux'
+import { increment } from './actions';
+import { decrement } from './actions';
 
 
 
 
+function App() {
 
-class App extends Component {
-
-
-render(){ 
+const counter = useSelector(state => state.counter);
+const isLogged = useSelector(state => state.isLogged);
+const dispatch = useDispatch();
 return (
     <div className='App'>
-       hi whats up 
+      <h1>Counter {counter}</h1> 
+      <button onClick={()=>dispatch(increment(5))}>+</button>
+      <button  onClick={()=>dispatch(decrement())}>-</button>
+      <h3>
+      {isLogged ? <h3>Valueable Information I should not see</h3>: 'No Information'}
+      </h3>
+      
       </div>
     
   );
-}
-}
+};
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import {createStore ,combineReducers} from 'redux';
 import allReducers from './reducers';
+import { Provider } from 'react-redux';
 
 const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
-
+<Provider store={store}>
   <App/>
+
+</Provider>
+  
 
   ,
   document.getElementById('root')
