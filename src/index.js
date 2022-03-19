9808e7f3867f7c27c946ae8eb141aa64a8da9a55
unknown <mdsakiluzzaman@gmail.com>
2022-03-19 10:32:30 +0600

    redux operations from scratch

--- src/index.js
+++ src/index.js
@@ -4,8 +4,35 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+import {createStore} from 'redux';
+
+const increment= ()=>{
+  return{
+    type:'INCREMENT'
+  }
+}
+const decrement= ()=>{
+  return{
+    type:'DECREMENT'
+  }
+}
+
+const counter = ( state = 0 ,action) =>{
+  switch(action.type){
+    case "INCREMENT":
+      return state+1;
+    case "DECREMENT":
+      return state -1;
+  }
+}
+let store = createStore(counter);
+
+store.subscribe(() => console.log(store.getState()));
 
 
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
 
 ReactDOM.render(
 
